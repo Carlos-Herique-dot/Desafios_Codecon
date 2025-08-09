100d693fc235faa5d2ef9de59aa5330da7dafa8e
Carlos-Herique-dot <carlosheriquephb2@outlook.com>
2025-08-09 08:52:29 -0300

    Versão 1.0.0
    Apenas escopo do Front

---
+++ Encurtador_URL/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Encurt</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div id="titulo">
+            <h1>Encurt URL</h1>
+            <p>Aqui você encurta seu URL de forma gratuita e sem burocracia</p>
+        </div>
+        <nav>
+            <a href="#">Inicio</a>
+            <a href="#">Quem somos</a>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <p>Desenvolvido por carlos.herique.dot</p>
+    </footer>
+    
+</body>
+</html>
